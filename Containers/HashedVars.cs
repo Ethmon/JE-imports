c7a8465bd6813580b26481436393360005ea3d20
Ethmon <Ethan6salu@gmail.com>
2024-11-08 10:34:17 -0500

    added the set stuff for chars

--- Containers/HashedVars.cs
+++ Containers/HashedVars.cs
@@ -185,7 +185,96 @@ namespace HashedVars
 		
 		public static void sett(List<string> equation, jumpE_basic.Data D, jumpE_basic.base_runner Base)
 		{
-			
+			if(equation[2] == "=")
+			{
+				
+				Jchar j = new Jchar(equation[3][0]);
+				D.setCustom("char",equation[1],j);
+			}
+			if (equation[2] == "BYT" && equation[3] == "=")
+			{
+				byte[] byt = (jumpE_basic.base_runner.dobyteMath(equation.Skip(4).ToArray(),D,Base));
+				Jchar j = new Jchar((char)byt[0]);
+				D.setCustom("char",equation[1],j);
+			}
+		}
+		public static void set(List<string> equation, jumpE_basic.Data D, jumpE_basic.base_runner Base)
+		{
+			if(equation[1] == "BYT")
+			{
+				byte[] bb = {0};
+				char math = (char)0;
+				if(equation.Count() != 3){
+					bb = (jumpE_basic.base_runner.dobyteMath(equation.Skip(3).ToArray(),D,Base));
+					math = (char)bb[0];
+				}
+				switch(equation[2])
+				{
+					case "=":
+						((Jchar)D.refrenceCustom("char",equation[0])).change(math);
+						break;
+					case "+=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() + math));
+						break;
+					case "-=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() - math));						
+						break;
+					case "*=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() * math));
+						break;
+					case "/=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() / math));
+						break;
+					case "++":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() + 1));						
+						break;
+					case "--":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() - 1));						
+						break;
+					case "**":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() * (char)((Jchar)D.refrenceCustom("char", equation[0])).get_value()));						
+						break;
+					default:
+						break;
+						
+				}
+				
+			}
+			else {
+				char math = (char)0;
+				if(equation.Count() != 2)
+					math = (char)(jumpE_basic.base_runner.doMath(equation.Skip(2).ToArray(),D,Base));
+				switch(equation[1])
+				{
+					case "=":
+						((Jchar)D.refrenceCustom("char",equation[0])).change(math);
+						break;
+					case "+=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() + math));
+						break;
+					case "-=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() - math));						
+						break;
+					case "*=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() * math));
+						break;
+					case "/=":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() / math));
+						break;
+					case "++":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() + 1));						
+						break;
+					case "--":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() - 1));						
+						break;
+					case "**":
+						((Jchar)D.refrenceCustom("char", equation[0])).change((char)(((Jchar)D.refrenceCustom("char", equation[0])).get_value() * (char)((Jchar)D.refrenceCustom("char", equation[0])).get_value()));						
+						break;
+					default:
+						break;
+						
+				}
+			}
 		}
 		
 		
